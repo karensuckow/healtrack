820314b2c52dad089d2ec31a04a37fab16db7166
Karen Suckow <karensuckow.dev@gmail.com>
2026-09-23 18:16:54 -0300

    chore: initial setup for Angular frontend and Spring Boot backend
    Includes Angular Material, Tailwind CSS v4, and base Spring Boot configuration.

---
+++ backend/src/main/java/com/github/karensuckow/healtrack/HealtrackApplication.java
@@ -0,0 +1,13 @@
+package com.github.karensuckow.healtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealtrackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/karensuckow/healtrack/HealtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.karensuckow.healtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
